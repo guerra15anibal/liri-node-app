8f909f3406a08b946b5b98f36fd6cfc169bb7c83
Anibal <guerra15anibal@hotmail.com>
2020-04-06 22:06:14 -0600

    got spotify to work

--- liri.js
+++ liri.js
@@ -1,25 +1,27 @@
 require("dotenv").config();
 
+var Spotify = require("node-spotify-api");
+
 var fs = require("fs");
 
-var spotify = new Spotify(keys.spotify);
-
 var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
 
 var input = process.argv[3];
-
+if (command === "spotify-this-song") {
+  songInfo(input);
+}
 function songInfo(input) {
-  var Spotify = require("node-spotify-api");
-
-  var spotify = new Spotify(keys.spotify);
-
   spotify.search({ type: "track", query: input }, function (err, data) {
     if (err) {
       return console.log("Error: " + err);
     }
-    console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
+
+    // console.log(JSON.parse(JSON.stringify(data.tracks.items[0].name)));
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
     console.log("Song Name: " + data.tracks.items[0].name);
     console.log("Spotify Preview Link: " + data.tracks.items[0].href);
     console.log("Album: " + data.tracks.items[0].album.name);
